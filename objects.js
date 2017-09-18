cdd050e4aabbac7881a69d030387147ab27c80d2
maude sos <mualth@gmail.com>
2017-09-18 00:58:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ var updatePlaylist = (playlist, artistName, songTitle) => {
 }
 
 var removeFromPlaylist = (playlist, artistName) => {
-  delete playlist.artistName;
+  delete playlist[artistName]
   return playlist
 
 }
